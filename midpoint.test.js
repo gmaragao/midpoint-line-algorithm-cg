20927a0840c3a4644b1a653865ce57e4d4219238
Gabriel Aragao <gabriel.aragao.comp@gmail.com>
2022-11-01 12:18:09 +0000

    test: add X test

--- midpoint.test.js
+++ midpoint.test.js
@@ -47,4 +47,27 @@ describe('Test lineMP', () => {
       { x: -2, y: -2 },
     ]);
   });
+
+  it('Should calculate correctly testing Y', () => {
+    let P = { x: 1, y: 2 };
+    let Q = { x: 8, y: 7 };
+
+    expect(lineMP(P, Q)).toStrictEqual([
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 3 },
+      { x: 4, y: 4 },
+      { x: 5, y: 5 },
+      { x: 6, y: 6 },
+      { x: 7, y: 6 },
+      { x: 8, y: 7 },
+    ]);
+  });
+
+  it('Should calculate correctly testing X', () => {
+    let P = { x: 2, y: 0 };
+    let Q = { x: 7, y: 6 };
+
+    lineMP(P, Q);
+  });
 });
